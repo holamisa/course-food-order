e76016c76c276afad4dc4903e8f378e32ec1a86f
holamisa <bongjaejeong@naver.com>
2024-03-10 22:13:04 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>You got this 💪</h1>
+      <p>Stuck? Not sure how to proceed?</p>
+      <p>
+        Don&apos;t worry - we&apos;ve all been there. Let&apos;s build it
+        together!
+      </p>
+    </>
+  );
+}
+
+export default App;
